397b77ceb823a5a23c7aea991cdbb5e8b33d4441
Angel May <Anmaychable@gmail.com>
2025-07-24 12:54:44 -0700

    Admin setup
    Setting up admin registration

--- menu/admin.py
+++ menu/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Bagel 
 
 # Register your models here.
+admin.site.register(Bagel)  # Register the Bagel model to make it available in the admin interface
