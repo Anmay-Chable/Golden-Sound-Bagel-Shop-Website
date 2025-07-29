df581c10fef6a169ab3ed72f1384ce2d730bf7f7
Angel May <Anmaychable@gmail.com>
2025-07-29 09:17:59 -0700

    Added Bagels to the database
    first bagels included in database

--- menu/views.py
+++ menu/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
+from .models import Bagel # Import the Bagel model to use in views
 
 # Create your views here.
 def homepage(request):
     """
     Render the homepage of the Golden Sound Bagel Shop.
     """
+    bagels = Bagel.objects.all()  # Fetch all Bagel objects from the database
+    context = {
+        'bagels': bagels  # Pass the bagels into a dictionary for the template
+    }
     return render(request, 'menu/index.html')
